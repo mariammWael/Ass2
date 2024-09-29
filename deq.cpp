e72db3da9ef7975ce3209643243b352e16f48c18
Mariam <YourEmailgit>
2024-09-30 00:08:13 +0300

    DEQ CPP file

--- deq.cpp
+++ deq.cpp
@@ -1,10 +1,5 @@
-//Mariam Abdelaal 900231510
+// DEQ implementation
 
-#include "deq.h"
-#include <stdexcept>
-using namespace std;
-
-//I made no assumptions, since I used a template to be as general as possible
 template <typename T>
 DEQ<T>::DEQ() : front(nullptr), rear(nullptr), length(0) {
 }
@@ -50,7 +45,7 @@ void DEQ<T>::Add_Rear(const T& value) {
 template <typename T>
 T DEQ<T>::Remove_Front() {
     if (DEQisEmpty()) {
-        throw runtime_error("Cannot remove from an empty DEQ");
+        throw std::runtime_error("Cannot remove from an empty DEQ");
     }
 
     Node* temp = front;
@@ -72,7 +67,7 @@ T DEQ<T>::Remove_Front() {
 template <typename T>
 T DEQ<T>::Remove_Rear() {
     if (DEQisEmpty()) {
-        throw runtime_error("Cannot remove from an empty DEQ");
+        throw std::runtime_error("Cannot remove from an empty DEQ");
     }
 
     Node* temp = rear;
@@ -94,7 +89,7 @@ T DEQ<T>::Remove_Rear() {
 template <typename T>
 T DEQ<T>::View_Front() const {
     if (DEQisEmpty()) {
-        throw out_of_range("No elements in DEQ");
+        throw std::out_of_range("No elements in DEQ");
     }
     return front->data;
 }
@@ -102,7 +97,7 @@ T DEQ<T>::View_Front() const {
 template <typename T>
 T DEQ<T>::View_Rear() const {
     if (DEQisEmpty()) {
-        throw out_of_range("No elements in DEQ");
+        throw std::out_of_range("No elements in DEQ");
     }
     return rear->data;
 }
@@ -110,26 +105,4 @@ T DEQ<T>::View_Rear() const {
 template <typename T>
 int DEQ<T>::DEQ_Length() const {
     return length;
-}
-
-template class DEQ<int>;
-
-int main() {
-    DEQ<int> deq;
-
-    deq.Add_Front(10);
-    deq.Add_Front(20);
-    deq.Add_Rear(30);
-    deq.Add_Rear(40);
-
-    cout << "DEQ Length: " << deq.DEQ_Length() << endl;
-    cout << "Front Element: " << deq.View_Front() << endl;
-    cout << "Removing Front: " << deq.Remove_Front() << endl;
-    cout << "New Front: " << deq.View_Front() << endl;
-
-    cout << "Rear Element: " << deq.View_Rear() << endl;
-    cout << "Removing Rear: " << deq.Remove_Rear() << endl;
-    cout << "New Rear: " << deq.View_Rear() << endl;
-
-    return 0;
 }
